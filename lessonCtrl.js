8b0f8fd8664e4c60bc6cb169090c3185133b80d6
Bryan Jackson <bjack13@gmail.com>
2016-12-20 16:53:41 -0700

    last push

--- lessonCtrl.js
+++ lessonCtrl.js
@@ -3,13 +3,17 @@ angular.module('directivePractice')
     $scope.test = 'Two-way data binding!'
     $scope.lessons = ['Services', 'Routing', 'Directives', 'Review', 'Firebase', 'No server project', 'Node', 'Express', 'Mongo'];
 
+
     $scope.announceDay = function (lesson , day) {
       if (!day) {
         alert(lesson + ' is not currently active.')
       }else {
         alert(lesson + ' is active on ' + day + '.')
       }
-
+    }
+    $scope.toggleStrikeOut = function functionName() {
+      toggle= !toggle;
+      return toggle;
     }
 
   })
